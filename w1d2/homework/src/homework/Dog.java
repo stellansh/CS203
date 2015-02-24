c6285bb54b659b009370cef9399e898e1035b6b6
stellansh <stellanshuti2@gmail.com>
2015-02-24 13:50:55 -0600

    Start of W1D2 homework

---
+++ w1d2/homework/src/homework/Dog.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework;
+
+/**
+ *
+ * @author stella
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+    
+}
---
+++ w1d2/homework/src/homework/Homework.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework;
+
+/**
+ *
+ * @author stella
+ */
+public class Homework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dog d1 = new Dog();
+        d1.breed = "Golden Retriever";
+        System.out.println(d1.breed);
+    }
+    
+}
