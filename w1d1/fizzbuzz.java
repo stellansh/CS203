ac982b822296b123db623e809bf8cc3c48184085
stellansh <stellanshuti2@gmail.com>
2015-02-23 23:05:29 -0600

    beer song code
    lowered the if statement after the printouts

---
+++ w1d1/fizzbuzz.java
@@ -0,0 +1,19 @@
+public class fizzbuzz{              
+    public static void main(String[]args){  
+      for(int i = 1; i <= 100; i++) {                    
+        if ((i % 3)==0){
+          System.out.println("fizz");
+        }
+        else if ((i % 5) == 0){
+          
+          System.out.println("buzz");
+     
+      }else if ((i % 15) == 0){ 
+          System.out.println("fizzbuzz");  
+                           
+      System.out.print(" "); 
+    }
+    System.out.println(i);
+  }
+    }
+}
