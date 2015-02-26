73b17e09a5e8da539df663ff2a8fe6e5fe07f066
stellansh <stellanshuti2@gmail.com>
2015-02-26 11:05:38 -0600

    ..

--- w1d3/homework/src/homework/Smallest.java
+++ w1d3/homework/src/homework/Smallest.java
@@ -10,23 +10,24 @@ package homework;
  * @author stella
  */
 public class Smallest {
+
     public static void main(String[] args) {
-        
-    double smallest =0;                    //double smallest=nums[0]
-       double[]nums= new double[100];
-       for (int i=0;i<nums.length;i++)
-       {
-         nums[i]=Math.random()*100;
-        smallest=nums[0];
-        if(nums[i]<smallest){
-            nums[i]=smallest;
-            
-        System.out.print(smallest);
+
+        double smallest = 100;                    //double smallest=nums[0]
+        double[] nums = new double[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.random() * 100;
+            //smallest = nums[0];
+            System.out.print(nums[i] + " ");
+            if (i == 0) {
+                smallest = nums[i];
+            }
+
+            if (nums[i] < smallest) {
+                smallest = nums[i];
+            }
         }
-          
-        
-       }
-        
-        
+        System.out.println();
+        System.out.print(smallest);
     }
 }
