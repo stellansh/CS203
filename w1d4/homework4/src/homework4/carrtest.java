e1e9822514998c09b283a8b5370265e2573132ae
stellansh <stellanshuti2@gmail.com>
2015-02-27 10:27:10 -0600

    homework

---
+++ w1d4/homework4/src/homework4/carrtest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework4;
+
+/**
+ *
+ * @author stella
+ */
+public class carrtest {
+     public static void main(String[] args){
+       carr myCar1 = new carr();
+       carr myCar2 = new carr();
+       
+         myCar1.setX(1);
+         myCar2.setX(3);
+         myCar1.setY(9);
+         myCar2.setY(6);
+       
+       myCar1.display();
+       myCar2.display();
+     }
+}
